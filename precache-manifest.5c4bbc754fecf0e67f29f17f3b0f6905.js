72e77b16f8794f02e14838c8959f1fd78b81aec6
Artem Kornev <artemkakornev1@gmail.com>
2019-08-10 22:11:44 +0300

    Updates

---
+++ precache-manifest.5c4bbc754fecf0e67f29f17f3b0f6905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fafd650708fe4d88aa61d0466a4eb42e",
+    "url": "/develops-test-app/index.html"
+  },
+  {
+    "revision": "52dd2cbad3938354d0ca",
+    "url": "/develops-test-app/static/css/main.7ab2accc.chunk.css"
+  },
+  {
+    "revision": "2f73cd8eeb8849abb051",
+    "url": "/develops-test-app/static/js/2.b0b32c26.chunk.js"
+  },
+  {
+    "revision": "52dd2cbad3938354d0ca",
+    "url": "/develops-test-app/static/js/main.55327208.chunk.js"
+  },
+  {
+    "revision": "35aee462863c237d4b49",
+    "url": "/develops-test-app/static/js/runtime~main.56a2f327.js"
+  }
+]);
